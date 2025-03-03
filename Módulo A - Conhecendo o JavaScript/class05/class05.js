89d302d343377a00b77d152245ef14061cd05df7
Alessandro Alves <alvesalessandro.school@gmail.com>
2025-03-03 10:29:04 -0300

    variables class

---
+++ Módulo A - Conhecendo o JavaScript/class05/class05.js
@@ -0,0 +1,7 @@
+// Variáveis e tipos primitivos
+
+var name = "Alessandro"
+var idade = 20
+var cidade = "Marituba"
+
+console.log("Meu nome é " + name + ", tenho " + idade + " e moro em " + cidade)
